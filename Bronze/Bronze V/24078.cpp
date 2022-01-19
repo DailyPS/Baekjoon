eba54f9cf9cd4f202e0fd1ce7dde109e01f3f14f
Dailycd <baksa1849@gmail.com>
2022-01-20 04:29:37 +0900

    Bronze V.

---
+++ Bronze/Bronze V/24078.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+int main(void)
+{
+  cin.tie(NULL);
+  ios::sync_with_stdio(false);
+
+  cin >> n;
+
+  cout << n % 21;
+
+  return 0;
+}
